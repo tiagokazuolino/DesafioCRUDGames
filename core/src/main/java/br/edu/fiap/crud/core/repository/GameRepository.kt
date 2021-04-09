f8d3099a41f5eedc43181e7f554ca8774797adc7
Tiago Kazuo Lino <tiagokazuolino@hotmail.com>
2021-04-08 23:51:51 -0300

    create a Game Repository and implement

--- core/src/main/java/br/edu/fiap/crud/core/repository/GameRepository.kt
+++ core/src/main/java/br/edu/fiap/crud/core/repository/GameRepository.kt
@@ -1,4 +1,13 @@
 package br.edu.fiap.crud.core.repository
 
-class GameRepository {
+import br.edu.fiap.crud.core.data.Game
+
+class GameRepository(private val dataSource: GameDataSource)  {
+    suspend fun addGame(game: Game) = dataSource.add(game)
+
+    suspend fun getGameById(id: Long) = dataSource.get(id)
+
+    suspend fun getGameList() = dataSource.getAll()
+
+    suspend fun removeGame(game: Game) = dataSource.remove(game)
 }
