7544263ad51184d09089485e539ac4056785d701
Tiago Kazuo Lino <tiagokazuolino@hotmail.com>
2021-04-10 03:51:38 -0300

    configure listview on click listener

--- app/src/main/java/br/edu/fiap/crud/games/presentation/ListFragment.kt
+++ app/src/main/java/br/edu/fiap/crud/games/presentation/ListFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavDirections
+import androidx.navigation.Navigation
 import br.edu.fiap.crud.games.R
+import kotlinx.android.synthetic.main.fragment_list.*
 
 class ListFragment : Fragment() {
 
@@ -14,5 +17,15 @@ class ListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        addGame.setOnClickListener { goToGameDetails() }
+    }
+
+    private fun goToGameDetails(id: Long = 0L) {
+        val action: NavDirections = ListFragmentDirections.actionGoToGame(id)
+        Navigation.findNavController(gamesListView).navigate(action)
     }
 }
