c4bade03ada63442f0b36d77b007b443ba933fed
Tiago Kazuo Lino <tiagokazuolino@hotmail.com>
2021-04-10 04:19:59 -0300

    resolve kotlin-android-extensions depricate using binding resolution

--- app/src/main/java/br/edu/fiap/crud/games/presentation/GameFragment.kt
+++ app/src/main/java/br/edu/fiap/crud/games/presentation/GameFragment.kt
@@ -7,19 +7,21 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
 import br.edu.fiap.crud.games.R
-import kotlinx.android.synthetic.main.fragment_game.*
+import br.edu.fiap.crud.games.databinding.FragmentGameBinding
 
 
 class GameFragment : Fragment() {
+    private lateinit var binding: FragmentGameBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentGameBinding.inflate(layoutInflater)
         return inflater.inflate(R.layout.fragment_game, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        checkButton.setOnClickListener { Navigation.findNavController(it).popBackStack() }
+        binding.checkButton.setOnClickListener { Navigation.findNavController(it).popBackStack() }
     }
 }
