de1d45e2b64b8a9e0643995a4992a186c5f35587
Tiago Kazuo Lino <tiagokazuolino@hotmail.com>
2021-04-12 20:28:43 -0300

    configuration MVVM and list data

---
+++ app/src/main/java/br/edu/fiap/crud/games/presentation/GamesListAdapter.kt
@@ -0,0 +1,50 @@
+package br.edu.fiap.crud.games.presentation
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import br.edu.fiap.crud.core.data.Game
+import br.edu.fiap.crud.games.databinding.ItemGameBinding
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
+
+class GamesListAdapter(var games: ArrayList<Game>) :
+    RecyclerView.Adapter<GamesListAdapter.GameViewHolder>() {
+    private var _binding: ItemGameBinding? = null
+
+    fun updateGames(newGames: List<Game>) {
+        games.clear()
+        games.addAll(newGames)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GameViewHolder {
+        _binding = ItemGameBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return GameViewHolder(_binding!!)
+    }
+
+    override fun onBindViewHolder(holder: GameViewHolder, position: Int) {
+        holder.bind(games[position])
+    }
+
+    override fun getItemCount(): Int = games.size
+
+    inner class GameViewHolder(view: ItemGameBinding) : RecyclerView.ViewHolder(view.root) {
+        private val layout = view.gameLayout
+        private val gameName = view.name
+        private val gameProducer = view.producer
+        private val gameNotes = view.notes
+        private val gameDate = view.date
+
+        fun bind(game: Game) {
+            gameName.text = game.name
+            gameProducer.text = game.producer
+            gameNotes.text = "${game.note}..."
+            val sdf = SimpleDateFormat("MMM dd, HH:mm")
+            val resultDate = Date(game.updateAt)
+            gameDate.text = "Last updated: ${sdf.format(resultDate)}"
+        }
+    }
+}
--- app/src/main/java/br/edu/fiap/crud/games/presentation/ListFragment.kt
+++ app/src/main/java/br/edu/fiap/crud/games/presentation/ListFragment.kt
@@ -5,17 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.NavDirections
 import androidx.navigation.Navigation
-import androidx.recyclerview.widget.RecyclerView
-import br.edu.fiap.crud.games.R
-import br.edu.fiap.crud.games.databinding.FragmentGameBinding
+import androidx.recyclerview.widget.LinearLayoutManager
 import br.edu.fiap.crud.games.databinding.FragmentListBinding
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import br.edu.fiap.crud.games.framework.ListViewModel
 
 class ListFragment : Fragment() {
     private var _binding: FragmentListBinding? = null
     private val binding get() = _binding!!
+    private val gamesListAdapter = GamesListAdapter(arrayListOf())
+    private lateinit var viewModel: ListViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,12 +29,36 @@ class ListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.gamesListView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = gamesListAdapter
+        }
+
         binding.addGame.setOnClickListener { goToGameDetails() }
+
+        viewModel = ViewModelProviders.of(this).get(ListViewModel::class.java)
+
+        observeViewModel()
     }
 
-    private fun goToGameDetails( id: Long = 0L) {
+    fun observeViewModel() {
+        viewModel.games.observe(viewLifecycleOwner, Observer { gamesList ->
+            binding.loadingView.visibility = View.GONE
+            binding.gamesListView.visibility = View.VISIBLE
+            gamesListAdapter.updateGames(gamesList.sortedBy { it.updateAt })
+            binding.gamesSizeView.text = "Size of Games list: ${gamesList.size}"
+        })
+    }
+
+    private fun goToGameDetails(id: Long = 0L) {
         val action: NavDirections = ListFragmentDirections.actionGoToGame(id)
         Navigation.findNavController(binding.gamesListView).navigate(action)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.getGames()
     }
 
     override fun onDestroyView() {
