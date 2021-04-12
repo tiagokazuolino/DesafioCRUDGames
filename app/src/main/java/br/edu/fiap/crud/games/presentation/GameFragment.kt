2b1e72ed7eea89fbc4f140f86a0753c28ca70dd2
Tiago Kazuo Lino <tiagokazuolino@hotmail.com>
2021-04-12 00:44:12 -0300

    fix: view bind in fragment navigation

--- app/src/main/java/br/edu/fiap/crud/games/presentation/GameFragment.kt
+++ app/src/main/java/br/edu/fiap/crud/games/presentation/GameFragment.kt
@@ -7,20 +7,28 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
 import br.edu.fiap.crud.games.R
+import br.edu.fiap.crud.games.databinding.FragmentGameBinding
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class GameFragment : Fragment() {
+    private var _binding: FragmentGameBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_game, container, false)
+        _binding = FragmentGameBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val checkButton =  view.findViewById<FloatingActionButton>(R.id.checkButton)
-        checkButton.setOnClickListener { Navigation.findNavController(it).popBackStack() }
+        binding.checkButton.setOnClickListener { Navigation.findNavController(it).popBackStack() }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
--- app/src/main/java/br/edu/fiap/crud/games/presentation/ListFragment.kt
+++ app/src/main/java/br/edu/fiap/crud/games/presentation/ListFragment.kt
@@ -9,25 +9,34 @@ import androidx.navigation.NavDirections
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import br.edu.fiap.crud.games.R
+import br.edu.fiap.crud.games.databinding.FragmentGameBinding
+import br.edu.fiap.crud.games.databinding.FragmentListBinding
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ListFragment : Fragment() {
+    private var _binding: FragmentListBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        _binding = FragmentListBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val addGame = view.findViewById<FloatingActionButton>(R.id.addGame)
-        addGame.setOnClickListener { goToGameDetails(view) }
+        binding.addGame.setOnClickListener { goToGameDetails() }
     }
 
-    private fun goToGameDetails(view: View, id: Long = 0L) {
+    private fun goToGameDetails( id: Long = 0L) {
         val action: NavDirections = ListFragmentDirections.actionGoToGame(id)
-        val gamesListView = view.findViewById<RecyclerView>(R.id.gamesListView)
-        Navigation.findNavController(gamesListView).navigate(action)
+        Navigation.findNavController(binding.gamesListView).navigate(action)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
